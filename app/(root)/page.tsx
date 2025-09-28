e06047d9683bb2494cbc2e1e7124e04234545bcc
Zaid-maker <53424436+Zaid-maker@users.noreply.github.com>
2025-09-29 00:31:07 +0500

    feat: add TradingViewWidget component and implement sign-up form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,22 @@
+import TradingViewWidget from "@/components/TradingViewWidget";
+
 const Home = () => {
   return (
     <div className="flex min-h-screen home-wrapper">
       <section className="grid w-full gap-8 home-section">
-        <div className="md:col-span-1 xl:col-span-1">TradingViewWidget</div>
-        <div className="md:col-span-1 xl:col-span-1">TraidingViewWidget</div>
+        <div className="md:col-span-1 xl:col-span-1">
+          <TradingViewWidget />
+        </div>
+        <div className="md:col-span-1 xl:col-span-1">
+          <TradingViewWidget />
+        </div>
       </section>
       <section className="grid w-full gap-8 home-section">
         <div className="h-full md:col-span-1 xl:col-span-1">
-          TradingViewWidget
+          <TradingViewWidget />
         </div>
         <div className="h-full md:col-span-1 xl:col-span-1">
-          TradingViewWidget
+          <TradingViewWidget />
         </div>
       </section>
     </div>
---
+++ components/TradingViewWidget.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React, { memo } from "react";
+
+const TradingViewWidget = () => {
+  return <div className="w-full">TradingViewWidget</div>;
+};
+
+export default memo(TradingViewWidget);
