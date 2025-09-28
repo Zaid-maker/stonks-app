fbaa133ba95cda7e885e16e3580d8049caf3e255
Zaid-maker <53424436+Zaid-maker@users.noreply.github.com>
2025-09-28 13:59:38 +0500

    root homepagfe

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
+    </div>
+  );
+};
+
+export default Home;
