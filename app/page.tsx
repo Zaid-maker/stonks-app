ea2337e645779512901f878133351f53b90f27c2
Zaid-maker <53424436+Zaid-maker@users.noreply.github.com>
2025-09-28 13:55:24 +0500

    update

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@/components/ui/button";
-
-export default function Home() {
-  return (
-    <main className="flex items-center justify-center h-screen">
-      <Button variant="default">Click Me</Button>
-    </main>
-  );
-}
